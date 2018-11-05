b233c008349fd276c1a7494593fa1cfada89a838
DaisyDare9 <44773087+DaisyDare9@users.noreply.github.com>
2018-11-05 14:21:26 +0200

    calculating_age

---
+++ calculateagedaisy.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+int main ()
+{
+
+    int number;
+    cout << "Enter your age, please\n";
+    cin >> number;
+    int NumberAddWith10 = number + 10;
+
+    cout << "Your age \n";
+    cout << number;
+    cout << "in ten years will be\n";
+    cout << NumberAddWith10;
+    cout << "\n";
+    return 0;
+}
